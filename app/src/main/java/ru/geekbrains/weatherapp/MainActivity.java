2f36ef098fd45ea22b9f10ecf6df3b97083052c5
Eugene Selavri <eugene.selavri@gmail.com>
2018-05-24 17:37:30 +0300

    add imageview/backgound; create my style

--- app/src/main/java/ru/geekbrains/weatherapp/MainActivity.java
+++ app/src/main/java/ru/geekbrains/weatherapp/MainActivity.java
@@ -10,7 +10,6 @@ import android.widget.CompoundButton;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    private Bundle bundle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +21,48 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText txt = (EditText) findViewById(R.id.enter_city);
-                CheckBox air = (CheckBox) findViewById(R.id.air_t);
                 Intent intent = new Intent(MainActivity.this, Main2Activity.class);
-                if (air.isChecked()) {
-                    intent.putExtra("air", true);
-                }
-                intent.putExtra("city", txt.getText().toString());
+                Bundle bundle = new Bundle();
+                bundle.putBoolean("air", hasAir());
+                bundle.putBoolean("water", hasWater());
+                bundle.putBoolean("pressure", hasPressure());
+                bundle.putBoolean("wind", hasSpeedOfWind());
+                bundle.putBoolean("wet", hasWet());
+                bundle.putString("city", whatCity().getText().toString());
+                intent.putExtras(bundle);
                 startActivity(intent);
             }
         });
 
+    }
+
+    private boolean hasAir() {
+        CheckBox air = (CheckBox) findViewById(R.id.air_t);
+        return air.isChecked();
+    }
+
+    private boolean hasWater() {
+        CheckBox water = (CheckBox) findViewById(R.id.water_t);
+        return water.isChecked();
+    }
+
+    private boolean hasPressure() {
+        CheckBox pressure = (CheckBox) findViewById(R.id.pressure);
+        return pressure.isChecked();
+    }
+
+    private boolean hasSpeedOfWind() {
+        CheckBox wind = (CheckBox) findViewById(R.id.wind);
+        return wind.isChecked();
+    }
+
+    private boolean hasWet() {
+        CheckBox wet = (CheckBox) findViewById(R.id.wet);
+        return wet.isChecked();
+    }
+
+    private EditText whatCity() {
+        EditText txt = (EditText) findViewById(R.id.enter_city);
+        return txt;
     }
 }
